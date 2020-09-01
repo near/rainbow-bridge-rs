c9ed46c61c586294d69142bf4ab9c7df339c225f
Bo Yao <bo@nearprotocol.com>
2020-09-01 10:48:09 -0700

    remove validate_ethash

--- eth-prover/tests/utils.rs
+++ eth-prover/tests/utils.rs
@@ -40,7 +40,6 @@ impl<'de> Deserialize<'de> for Hex {
 
 #[derive(BorshSerialize)]
 struct EthClientInitArgs {
-    validate_ethash: bool,
     dags_start_epoch: u64,
     dags_merkle_roots: Vec<H128>,
     first_header: Vec<u8>,
@@ -169,11 +168,9 @@ impl ExternalUser {
         &self,
         runtime: &mut RuntimeStandalone,
         eth_client_account_id: AccountId,
-        validate_ethash: bool,
     ) -> TxResult {
         let block = read_block("../eth-client/src/data/10234001.json".to_string());
         let init_args = EthClientInitArgs {
-            validate_ethash,
             dags_start_epoch: 0,
             dags_merkle_roots: read_roots_collection().dag_merkle_roots,
             first_header: block.header(),
